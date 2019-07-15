68d88c9ee36affec1965aeeb5f28d4e57f980318
ballsai <jarunemail@gmail.com>
2019-07-15 15:46:05 +0700

    Update ExcelWrite.py

--- ExcelWrite.py
+++ ExcelWrite.py
@@ -2,59 +2,64 @@ import os
 import re
 from glob import glob
 import pandas as pd
-files_list = []
-device_detail = []
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
-def readEachLine(file):
-    device_name = ''
-    device_model = ''
-    col = []
-    # for line in file:
-    #     if re.search('.*#show ver.*', line):
-    #         device_name = re.sub('#show ver.*','',line)
-    #     if re.search('^Model ([n]|[N])umber.*:', line):
-    #         device_model = re.sub('^Model ([n]|[N])umber.*:','',line)
-    #         # print(device_name + device_model)
-    #     if re.search('.*#sh.*int.*desc.*', line):
-    #         lineSection = True:
-    while True:
-        line = file.readline()
-        if not line:
-            break
-        if re.search('.*#sh.*int.*desc.*', line):
-            line = file.readline()
-            col = line.split()
-            # print(col)
-            device_detail.append(col)  
-            continue
-        if re.search('^[FGVTP]', line) and col :      
-            element = re.split(r'\s{2,}',line)        # split into list as an element at least 2 whitespaces
-            device_detail.append(element)    
-            if not re.search('^[FGVTP]', file.readline()):
-                break
-    # for detail in device_detail:
-    #     print(detail)
+cols = []
+cols2 = []
+cells = []
+cells2 = []
 
-def readEachFile(files_list):
-    for file_name in files_list:
-        try:
-            with open(file_name, 'r') as file: 
-                readEachLine(file)
-        except IOError as exc:
-            if exc.errno != errno.EISDIR:
-                raise
+filename = '../All Configure/LS_3560_02,no-ip,console,1_all,.txt'
 
 
-def matchFilename(path, pattern_list):
-    for pattern in pattern_list:
-        files_list.extend( glob( path + pattern ))
 
+try:
+    with open(filename, 'r') as f:
+        lines = [line for line in f.readlines() if line.strip()]
+except IOError as exc:
+    if exc.errno != errno.EISDIR:
+        raise
 
-path = '../All Configure/'                                     # enter path name here
-# pattern_list = ['LS_3560_02,no-ip,console,1_all,.txt']         # enter filename pattern here
-pattern_list = ['*1_all*', '*step1*']         # enter filename pattern here
-matchFilename(path, pattern_list)       # search matching filename
-readEachFile(files_list)                # read each matching file
+for i in range(0, len(lines)):
+    if not lines[i]:
+        break 
+    if re.search('.*#sh.*int.*desc.*', lines[i]):
+        i += 1
+        cols.extend(lines[i].split())
+        i += 1
+        while not re.search('.*#show.*', lines[i]):
+            cells.append(re.split(r'\s{2,}',lines[i])) 
+            i += 1
 
-for detail in device_detail:
-    print(detail)
+    if re.search('.*#sh.*int.*status$', lines[i]):
+        i += 1
+        cols2.extend(lines[i].split())
+        i += 1
+        while not re.search('.*#show.*', lines[i]):
+            cells2.append(re.split(r'\s{2,}',lines[i])) 
+            i += 1   
+
+df1 = pd.DataFrame(cells)
+df2 = pd.DataFrame(cells2)
+
+df1.rename(columns = {0:'Interface',
+                    1:'Status',
+                    2:'Protocol',
+                    3:'Description' }, 
+            inplace=True)
+df2.rename(columns = {0:'Port',
+                    1:'Name',
+                    2:'Status',
+                    3:'Vlan',
+                    4:'Duplex',
+                    5:'Speed',
+                    6:'Type' }, 
+            inplace=True)
+
+df3 = pd.merge(df1, df2, left_on='Interface', right_on='Port')
+print(df3)
+writer = ExcelWriter('test0.xlsx')
+df3.to_excel(writer,'Sheet1',index=False)
+writer.save()
+
